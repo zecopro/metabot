e276fa3cf70959c62031cfdb009219e8004b22e1
zecopro <zecoa555@gmail.com>
2020-11-05 01:11:07 +0300

    Update metabot.sh

--- metabot.sh
+++ metabot.sh
@@ -4,7 +4,7 @@ masscan  -p #target_port 0.0.0.0/0 --rate=99999 --exclude 255.255.255.255 -oX sm
 
 sleep 5
 
-cat /var/.,/smb.xml | grep addr | cut -d '"' -f 4 > /var/.,/ips.txt 
+cat smb.xml | grep addr | cut -d '"' -f 4 > ips.txt 
 
 msfconsole -x 'setg RHOSTS file:ips.txt; setg THREADS 400; setg RPORT #target_port; use #auxiliary_meta_module_to_find_hosts_vulberability; run; exit;'
 
